1842a492f537d5aff0110db669abf2dce92fbc45
MHMighani <mh.mgh2012@gmail.com>
2022-08-21 11:58:27 +0430

    feat: sub menu on-hover appearance

--- src/components/layout/navMenu.jsx
+++ src/components/layout/navMenu.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react";
 import MobileAuthLinks from "./mobileAuthLinks";
 import SubMenu from "./subMenu";
+
 function NavMenu({ navMenuItems }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleClass(element, className) {
+    element.classList.toggle(className);
+  }
+
+  function handleNavItemClick(e) {
+    setIsOpen(!isOpen);
+    e.currentTarget.classList.toggle("active");
+    toggleClass(e.currentTarget, "active");
+  }
+
+  function handleNavItemEnter(e) {
+    setIsOpen(true);
+    toggleClass(e.currentTarget, "active");
+  }
+
+  function handleNavItemLeave(e) {
+    setIsOpen(false);
+    toggleClass(e.currentTarget, "active");
+  }
+
   return (
     <nav className="nav">
-      <ul className="nav__menu">
+      <ul className={`nav__menu ${isOpen ? "nav__menu--opened" : ""}`}>
         {navMenuItems.map((item) => (
-          <li className="nav-item" key={item.id}>
+          <li
+            onMouseEnter={handleNavItemEnter}
+            onMouseLeave={handleNavItemLeave}
+            onClick={handleNavItemClick}
+            className="nav-item"
+            key={item.id}
+          >
             {item.type === "link" ? (
               <a className="nav-item__label" href={item.link}>
                 {item.label}
