18f411b03f112d271b6f02b593433ecf872af008
MHMighani <mh.mgh2012@gmail.com>
2022-08-22 11:40:55 +0430

    chore: seperate pages

--- src/components/pages/landingPage.jsx
+++ src/components/pages/landingPage.jsx
@@ -1,7 +1,21 @@
 import Navbar from "../layout/navbar";
+import HeroSection from "../heroSection";
+import SectionSeperator from "../../assets/section-seperator";
+import BusinessProgramSection from "../businessProgramSection";
+
+import "./generalSectionStyles.scss";
 
 function LandingPage() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <div>
+        <HeroSection />
+        <SectionSeperator previousColor="#fff" nextColor="#f6f6f7" />
+        <BusinessProgramSection />
+      </div>
+    </>
+  );
 }
 
 export default LandingPage;
