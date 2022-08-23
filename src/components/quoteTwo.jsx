a85e39d82fb438c10b506f7b8a1d8917d92ae546
MHMighani <mh.mgh2012@gmail.com>
2022-08-23 20:11:13 +0430

    feat: initialize second quote section

---
+++ src/components/quoteTwo.jsx
@@ -0,0 +1,15 @@
+import QuoteSection from "./common/quoteSection";
+import QuoteTwoImage from "../assets/quote-2.jpg";
+
+function QuoteTwo() {
+  const quoteDetails = {
+    main: "In this partnership, I was simply looking for new streams of revenue. A great thing is that I also found lots of co-marketing opportunities and amazing support.",
+    image: QuoteTwoImage,
+    authorName: "Syed Balkhi",
+    authorDetail: "Owner at WPBeginner",
+  };
+
+  return <QuoteSection quoteDetails={quoteDetails} />;
+}
+
+export default QuoteTwo;
