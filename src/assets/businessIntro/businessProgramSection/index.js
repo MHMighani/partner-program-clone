eb29d03ee411e914fcdfb309bf795a993f621fa7
MHMighani <mh.mgh2012@gmail.com>
2022-08-25 19:04:33 +0430

    chore: move assets move assets to seperate them from dashboard assets

---
+++ src/assets/businessIntro/businessProgramSection/index.js
@@ -0,0 +1,4 @@
+export { default as BlobSvg } from "./blob.svg";
+export { default as ImageDestkop1 } from "./business-1.jpg";
+export { default as ImageDestkop2 } from "./business-2.jpg";
+export { default as ImageTablet } from "./business-tablet.jpg";
