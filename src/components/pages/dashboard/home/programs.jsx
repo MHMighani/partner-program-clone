434b6e7c35763ff62044b0849841d1df1cd16903
MHMighani <mh.mgh2012@gmail.com>
2022-08-29 23:20:51 +0430

    style: programs

--- src/components/pages/dashboard/home/programs.jsx
+++ src/components/pages/dashboard/home/programs.jsx
@@ -29,43 +29,62 @@ function CardTitle({ title }) {
 }
 
 function BalanceCard() {
+  const btnLabel = "Widraw money";
   return (
-    <div className="program-card">
+    <div className="program-card balance">
       <CardTitle title="Balance" />
-      <span>
-        <span>$0.00</span>
-      </span>
-      <div>
-        <span>total earnings</span>
+      <div className="desktop btn btn--link">{btnLabel}</div>
+
+      <div className="values">
         <span>
+          <span className="balance-value">$0.00</span>
+        </span>
+        <span>
+          <span>total earnings</span>
           <span>$0.00</span>
         </span>
       </div>
 
-      <div>
-        <button>withdraw money</button>
+      <div className="mobile">
+        <button className="btn btn--outline">{btnLabel}</button>
       </div>
     </div>
   );
 }
 
 function AffiliateCard() {
+  const getLinkLabel = "Get link";
+  const campaignLabel = "Create a campaign";
   return (
-    <div className="program-card">
-      <div>
-        <CardTitle title="Affiliate" />
-        <InlineList />
+    <div className="program-card affiliate">
+      <CardTitle title="Affiliate" />
+      <div className="desktop">
+        <button className="btn btn--link">{getLinkLabel}</button>
+        <button className="btn btn--link">{campaignLabel}</button>
+      </div>
+      <InlineList />
+      <div className="mobile">
+        <button className="btn btn--blue">{getLinkLabel}</button>
+        <button className="btn btn--outline">{campaignLabel}</button>
       </div>
     </div>
   );
 }
 
 function SolutionCard() {
+  const btnLabel = "Create a license";
   return (
-    <div className="program-card">
+    <div className="program-card solution">
       <CardTitle title="Solution" />
+      <div className="desktop">
+        <button className="btn btn--link">{btnLabel}</button>
+      </div>
       <InlineList />
-      <button>Create a license</button>
+      <div className="mobile">
+        <button className="btn btn--blue btn--blue--disabled">
+          {btnLabel}
+        </button>
+      </div>
     </div>
   );
 }
