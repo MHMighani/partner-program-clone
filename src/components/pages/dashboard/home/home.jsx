094e45ad74b32f193defb03081a5e64a56c42d4b
MHMighani <mh.mgh2012@gmail.com>
2022-08-29 23:42:03 +0430

    feat: add home title

--- src/components/pages/dashboard/home/home.jsx
+++ src/components/pages/dashboard/home/home.jsx
@@ -1,6 +1,7 @@
 import Programs from "./programs";
 import Performance from "./performance";
 import Partnership from "./partnership";
+import Explorer from "./explorer";
 
 import "./home.scss";
 
@@ -8,6 +9,8 @@ function Home() {
   return (
     <section className="section">
       <div className="home-section">
+        <div className="home-section__title">Home</div>
+        <Explorer />
         <Programs />
         <Performance />
         <Partnership />
