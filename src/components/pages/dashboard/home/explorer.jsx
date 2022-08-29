0a3e258bad357d500a733f245ac6a50f254fdf6c
MHMighani <mh.mgh2012@gmail.com>
2022-08-29 19:28:07 +0430

    feat: add explorer mobile

---
+++ src/components/pages/dashboard/home/explorer.jsx
@@ -0,0 +1,102 @@
+import ExploreDesktop from "./exploreDesktop";
+import ExplorerMobile from "./explorerMobile";
+import useIsDesktop from "../../../../hooks/useIsDesktop";
+
+import explorer1 from "../../../../assets/dashboard/home/explorer-1.png";
+import explorer2 from "../../../../assets/dashboard/home/explorer-2.png";
+import explorer3 from "../../../../assets/dashboard/home/explorer-3.png";
+import explorer4 from "../../../../assets/dashboard/home/explorer-4.png";
+import explorer5 from "../../../../assets/dashboard/home/explorer-5.png";
+import explorer6 from "../../../../assets/dashboard/home/explorer-6.png";
+import finished from "../../../../assets/dashboard/home/finished.png";
+
+const data = [
+  {
+    id: "signin",
+    listTitle: "Sign in to LiveChat Partner Program",
+    contentTitle: "Welcome to LiveChat Partnerships 👋",
+    description:
+      " Explore the application and start growing your business with LiveChat Partner Program. ",
+    linkText: "Let’s get started",
+    image: explorer1,
+    checked: false,
+  },
+  {
+    id: "discover",
+    listTitle: "Discover our products",
+    contentTitle: "Welcome to LiveChat Partnerships 👋",
+    description:
+      " Your clients and our products are a perfect match! See what products you can build your business with. ",
+    linkText: "See our products",
+    image: explorer2,
+    checked: false,
+  },
+  {
+    id: "links",
+    listTitle: "Get your referral links",
+    contentTitle: "Get your referral link for selected product",
+    description:
+      "  Earn recurring revenue when you refer merchants to LiveChat, ChatBot or to our program itself.  ",
+    linkText: "Get link",
+    image: explorer3,
+    checked: false,
+  },
+  {
+    id: "uptodate",
+    listTitle: "Stay up to date",
+    contentTitle: "Stay up to date, watch “The Bigger Picture",
+    description:
+      " Watch our video series, learn about new releases and get insights on how to implement them into your business. ",
+    linkText: "Watch video",
+    image: explorer4,
+    checked: false,
+  },
+  {
+    id: "solution",
+    listTitle: "Apply to Solution Program",
+    contentTitle: "Apply to Solution Program",
+    description:
+      "  Earn recurring revenue from direct recommendations or resell LiveChat on your own terms. Receive badges and get listed on LiveChatExperts Marketplace. ",
+    linkText: "Apply",
+    image: explorer5,
+    checked: false,
+  },
+  {
+    id: "license",
+    listTitle: "Create first license for client",
+    contentTitle: "Create first license for client",
+    description:
+      "   Setting up new licenses in Partner App is easy. Create one for your client or simply run a quick test!  ",
+    linkText: "Apply to Solution Partner Program",
+    last: true,
+    image: explorer6,
+    checked: false,
+  },
+];
+
+const congratsCardData = {
+  id: "congrats",
+  listTitle: "Congrats!",
+  contentTitle: "Congrats! You know the App inside out!",
+  description: (
+    <>
+      Got any questions?
+      <br />
+      Chat with support or shoot us email.
+    </>
+  ),
+  linkText: "Finish onboarding",
+  last: false,
+  image: finished,
+};
+
+function Explorer() {
+  const isDesktop = useIsDesktop();
+  return isDesktop ? (
+    <ExploreDesktop data={data} />
+  ) : (
+    <ExplorerMobile data={data} />
+  );
+}
+
+export default Explorer;
---
+++ src/components/pages/dashboard/home/explorerMobile.jsx
@@ -0,0 +1,54 @@
+import Popup from "reactjs-popup";
+import { useState } from "react";
+import Dots from "react-carousel-dots";
+
+function ExploreTrigger() {
+  return (
+    <div className="explorer-mobile__trigger">Get to know Partner Program</div>
+  );
+}
+
+function ExplorerModal({ item, selected, handleNext }) {
+  return (
+    <div className="explorer-mobile__body">
+      <Dots length={6} visible={6} active={selected} />
+      <div key={item.id} className="card">
+        <b>{item.contentTitle}</b>
+        <img className="card__image" src={item.image} alt={item.title} />
+        <p className="card__description">{item.description}</p>
+        <button onClick={handleNext} className="btn btn--blue">
+          {item.linkText}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function ExplorerMobile({ data }) {
+  const [selected, setSelected] = useState(0);
+
+  return (
+    <Popup
+      trigger={
+        <div className="explorer-mobile">
+          <ExploreTrigger />
+        </div>
+      }
+    >
+      {(close) => (
+        <div className="explorer-mobile__modal">
+          <button className="explorer-mobile__closeBtn" onClick={close}>
+            &times;
+          </button>
+          <ExplorerModal
+            handleNext={() => setSelected(selected + 1)}
+            selected={selected}
+            item={data[selected]}
+          />
+        </div>
+      )}
+    </Popup>
+  );
+}
+
+export default ExplorerMobile;
