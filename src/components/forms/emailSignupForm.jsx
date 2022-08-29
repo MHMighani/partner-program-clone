f09e980ab76a280a81026ac10e7504d33cb158ca
MHMighani <mh.mgh2012@gmail.com>
2022-08-29 09:46:06 +0430

    fix: email input submit

--- src/components/forms/emailSignupForm.jsx
+++ src/components/forms/emailSignupForm.jsx
@@ -1,12 +1,24 @@
+import { useRef } from "react";
+
 import "./emailSignupForm.scss";
 
 function EmailSignupForm({ submitBtnText }) {
+  const emailInputRef = useRef();
+
+  // handle email submit
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(emailInputRef.current.value);
+  }
+
   return (
-    <form className="email-signup-form">
+    <form onSubmit={handleSubmit} className="email-signup-form">
       <input
         type="email"
         className="email-input"
         placeholder="Enter your email"
+        ref={emailInputRef}
       />
       <button className="btn btn--red">{submitBtnText}</button>
     </form>
